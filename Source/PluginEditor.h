955d9d2ad95204a110e691aa9e30c0a5e5858321
Ryan <ryanloweth29@gmail.com>
2021-05-23 19:28:28 -0700

    configured sliders

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,6 +11,14 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct CustomRotarySlider : juce::Slider
+{
+    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+        
+    }
+};
+
 //==============================================================================
 /**
 */
@@ -28,6 +36,16 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     EQpluginAudioProcessor& audioProcessor;
+    
+    CustomRotarySlider peakFreqSlider,
+    peakGainSlider,
+    peakQualitySlider,
+    lowCutFreqSlider,
+    lowCutSlopeSlider,
+    highCutFreqSlider,
+    highCutSlopeSlider;
+    
+    std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EQpluginAudioProcessorEditor)
 };
