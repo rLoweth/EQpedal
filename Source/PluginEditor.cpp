6a9202ba27d37d6430bcfdabe61d4e459f110ab2
Ryan <ryanloweth29@gmail.com>
2021-05-24 13:00:53 -0700

    created base for custom sliders, no visuals yet

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -130,7 +130,16 @@ void ResponseCurveComponent::paint (juce::Graphics& g)
 
 //==============================================================================
 EQpluginAudioProcessorEditor::EQpluginAudioProcessorEditor (EQpluginAudioProcessor& p)
-: AudioProcessorEditor (&p), audioProcessor (p), responseCurveComponent(audioProcessor),
+: AudioProcessorEditor (&p), audioProcessor (p),
+peakFreqSlider(*audioProcessor.apvts.getParameter("Peak Freq"), "Hz"),
+peakGainSlider(*audioProcessor.apvts.getParameter("Peak Gain"), "dB"),
+peakQualitySlider(*audioProcessor.apvts.getParameter("Peak Quality"), ""),
+lowCutFreqSlider(*audioProcessor.apvts.getParameter("LowCut Freq"), "Hz"),
+lowCutSlopeSlider(*audioProcessor.apvts.getParameter("LowCut Slope"), "dB"),
+highCutFreqSlider(*audioProcessor.apvts.getParameter("HighCut Freq"), "Hz"),
+highCutSlopeSlider(*audioProcessor.apvts.getParameter("HighCut Slope"), "dB"),
+
+responseCurveComponent(audioProcessor),
 peakFreqSliderAttachment(audioProcessor.apvts, "Peak Freq", peakFreqSlider),
 peakGainSliderAttachment(audioProcessor.apvts, "Peak Gain", peakGainSlider),
 peakQualitySliderAttachment(audioProcessor.apvts, "Peak Quality", peakQualitySlider),
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,12 +11,40 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-struct CustomRotarySlider : juce::Slider
+struct LookAndFeel : juce::LookAndFeel_V4
 {
-    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox)
+    void drawRotarySlider (juce::Graphics&,
+                                   int x, int y, int width, int height,
+                                   float sliderPosProportional,
+                                   float rotaryStartAngle,
+                                   float rotaryEndAngle,
+                                   juce::Slider&) override { }
+    
+};
+
+struct RotarySliderLabeled : juce::Slider
+{
+    RotarySliderLabeled(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox),param(&rap), suffix(unitSuffix)
     {
-        
+        setLookAndFeel(&lnf);
     }
+    
+    ~RotarySliderLabeled()
+    {
+        setLookAndFeel(nullptr);
+    }
+    
+    void paint(juce::Graphics& g) override { };
+    juce::Rectangle<int> getSliderBounds() const;
+    int getTextHight() const { return 14; };
+    juce::String getDisplayString() const;
+    
+    
+private:
+    LookAndFeel lnf;
+
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
 };
 
 struct ResponseCurveComponent: juce::Component, juce::AudioProcessorParameter::Listener, juce::Timer
@@ -58,7 +86,7 @@ private:
     // access the processor object that created it.
     EQpluginAudioProcessor& audioProcessor;
     
-    CustomRotarySlider peakFreqSlider,
+    RotarySliderLabeled peakFreqSlider,
     peakGainSlider,
     peakQualitySlider,
     lowCutFreqSlider,
