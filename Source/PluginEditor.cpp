7b151b0684920699e634f58fcf7575b0b4bccaa8
Ryan <ryanloweth29@gmail.com>
2021-05-25 19:46:54 -0700

    added visuals for music playing

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -228,13 +228,33 @@ void ResponseCurveComponent::timerCallback()
         }
     }
     
+    const auto fftBounds = getAnalysisArea().toFloat();
+    
+    const auto fftSize = leftChannelFFTDataGenerator.getFFTSize();
+    
+    const auto binWidth = audioProcessor.getSampleRate() / (double)fftSize;
+    
+    while( leftChannelFFTDataGenerator.getNumAvailableFFTDataBlocks() > 0 )
+    {
+        std::vector<float> fftData;
+        if( leftChannelFFTDataGenerator.getFFTData(fftData) )
+        {
+            pathProducer.generatePath(fftData, fftBounds, fftSize, binWidth, -48.f);
+        }
+    }
+    
+    while( pathProducer.getNumPathsAvailable() )
+    {
+        pathProducer.getPath(leftChannelFFTPath);
+    }
+    
     if( parametersChanged.compareAndSetBool(false, true) )
     {
 
         updateChain();
-        
-        repaint();
     }
+    
+    repaint();
 }
 
 void ResponseCurveComponent::updateChain()
@@ -319,6 +339,9 @@ void ResponseCurveComponent::paint (juce::Graphics& g)
     {
         responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
     }
+    
+    g.setColour(Colours::blue);
+    g.strokePath(leftChannelFFTPath, PathStrokeType(1.f));
     
     g.setColour(Colours::orange);
     g.drawRoundedRectangle(getRenderArea().toFloat(), 4.f, 1.f);
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -94,6 +94,72 @@ private:
     Fifo<BlockType> fftDataFifo;
 };
 
+template<typename PathType>
+struct AnalyzerPathGenerator
+{
+    /*
+     converts 'renderData[]' into a juce::Path
+     */
+    void generatePath(const std::vector<float>& renderData,
+                      juce::Rectangle<float> fftBounds,
+                      int fftSize,
+                      float binWidth,
+                      float negativeInfinity)
+    {
+        auto top = fftBounds.getY();
+        auto bottom = fftBounds.getHeight();
+        auto width = fftBounds.getWidth();
+
+        int numBins = (int)fftSize / 2;
+
+        PathType p;
+        p.preallocateSpace(3 * (int)fftBounds.getWidth());
+
+        auto map = [bottom, top, negativeInfinity](float v)
+        {
+            return juce::jmap(v,
+                              negativeInfinity, 0.f,
+                              float(bottom+10),   top);
+        };
+
+        auto y = map(renderData[0]);
+        if( std::isnan(y) || std::isinf(y) )
+            y = bottom;
+        
+        p.startNewSubPath(0, y);
+
+        const int pathResolution = 2;
+
+        for( int binNum = 1; binNum < numBins; binNum += pathResolution )
+        {
+            y = map(renderData[binNum]);
+
+            if( !std::isnan(y) && !std::isinf(y) )
+            {
+                auto binFreq = binNum * binWidth;
+                auto normalizedBinX = juce::mapFromLog10(binFreq, 20.f, 20000.f);
+                int binX = std::floor(normalizedBinX * width);
+                p.lineTo(binX, y);
+            }
+        }
+
+        pathFifo.push(p);
+    }
+
+    int getNumPathsAvailable() const
+    {
+        return pathFifo.getNumAvailableForReading();
+    }
+
+    bool getPath(PathType& path)
+    {
+        return pathFifo.pull(path);
+    }
+private:
+    Fifo<PathType> pathFifo;
+};
+
+
 struct LookAndFeel : juce::LookAndFeel_V4
 {
     void drawRotarySlider (juce::Graphics&,
@@ -171,6 +237,10 @@ private:
     juce::AudioBuffer<float> monoBuffer;
     
     FFTDataGenerator<std::vector<float>> leftChannelFFTDataGenerator;
+    
+    AnalyzerPathGenerator<juce::Path> pathProducer;
+    
+    juce::Path leftChannelFFTPath;
 };
 
 //==============================================================================
